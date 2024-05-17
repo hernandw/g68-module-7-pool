2eaafb2ff082c7e6cd9d67dceef9a9ba2cfa5770
Williams Hernandez <hernandw@gmail.com>
2024-05-16 21:55:29 -0400

    :sparkles: creacion de la tabla productos

--- config/db.js
+++ config/db.js
@@ -1,17 +1,25 @@
-import pg from 'pg';
-import 'dotenv/config'
+import pg from "pg";
+import "dotenv/config";
 const { Pool } = pg;
 
-const { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD } = process.env
+const { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD } = process.env;
 
 const config = {
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB_DATABASE,
-    password: DB_PASSWORD,
-   allowExitOnIdle: true
-}
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  allowExitOnIdle: true,
+  
+};
 
-export const pool = new Pool(config);
+const pool = new Pool(config);
 
+const getDate = async () => {
+  const { rows } = await pool.query("SELECT NOW()");
+  console.log(rows[0].now);
+};
 
+getDate();
+
+export default pool;
--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {pool} from "../config/db.js"
+import pool from "../config/db.js"
 const router = express.Router();
 
 
@@ -17,6 +17,13 @@ res.send(sql.rows)
 
 })
 
+
+router.get('/products', async (req, res) => {
+
+const sql = await pool.query("SELECT * FROM products")
+console.log(sql.rows)
+res.send(sql.rows)
+})
 
 
 router.get('*', (req, res) => {
